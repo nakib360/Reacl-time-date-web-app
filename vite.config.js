e0b8921472e34f0a12d3c9ae86a4546ce1c1317a
Muhammad Nakib Uddin <fliptechofficial@gmail.com>
2026-03-06 10:48:19 +0000

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Date Widget",
+        short_name: "Dates",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#7e5bef",
+        icons: [
+          {
+            src: "pwa-icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
     tailwindcss(),
     react()
   ],
